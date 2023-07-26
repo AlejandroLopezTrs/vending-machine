bc99a78f11d479d2ec75dca7347c3109d05c6e38
Daniel Commins <daniel.commins@techelevator.com>
2023-07-26 16:57:01 -0700

    nlr-15 refactor

--- src/main/java/com/techelevator/VendingMachineCLI.java
+++ src/main/java/com/techelevator/VendingMachineCLI.java
@@ -1,36 +1,36 @@
 package com.techelevator;
 
-// Menu is provided to you as a suggested class to handle user input
-// Build out a menu class to start
-
+/*
+ * This class is provided to you as a *suggested* class to start
+ * your project. Feel free to refactor this code as you see fit.
+ */
 public class VendingMachineCLI {
 
 	private static final String MAIN_MENU_OPTION_DISPLAY_ITEMS = "Display Vending Machine Items";
 	private static final String MAIN_MENU_OPTION_PURCHASE = "Purchase";
 	private static final String[] MAIN_MENU_OPTIONS = { MAIN_MENU_OPTION_DISPLAY_ITEMS, MAIN_MENU_OPTION_PURCHASE };
 
-	private Menu menu;
-
-	public VendingMachineCLI(Menu menu) {
-		this.menu = menu;
-	}
-
 	public static void main(String[] args) {
-		// You will need to create a Menu class to handle display.
-		//Menu menu = new Menu();
-		VendingMachineCLI cli = new VendingMachineCLI(menu);
+		VendingMachineCLI cli = new VendingMachineCLI();
 		cli.run();
 	}
 
 	public void run() {
+
 		while (true) {
-			// Use a method from your Menu class to initialize this value
-			String choice = "initalize this here";
+			/*
+			 * TODO: Use a method to initialize this value
+			 */
+			String choice = "";
 
 			if (choice.equals(MAIN_MENU_OPTION_DISPLAY_ITEMS)) {
+
 				// display vending machine items
+
 			} else if (choice.equals(MAIN_MENU_OPTION_PURCHASE)) {
+
 				// do purchase
+
 			}
 		}
 	}
